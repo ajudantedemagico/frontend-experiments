f2d1fb9594cfeb944d342a1917e0fd53ee0dee23
ajudantedemagico <jqlngnclvz@gmail.com>
2026-02-18 17:26:55 -0300

    feat: javascript button logic

--- gatinhoteca/js/script.js
+++ gatinhoteca/js/script.js
@@ -1,1 +1,24 @@
-console.log("JS carregou");
+let estadoDoGato = 'neutro';
+
+const botaoTenho = document.querySelector (".botao-tenho");
+const imagemGato = document.querySelector('.img-gato');
+
+
+
+botaoTenho.addEventListener('click', function () {
+  console.log ('Cliquei em Tenho');
+  estadoDoGato = 'tenho';
+  imagemGato.src = '/gatinhoteca/assets/img/github.png';
+
+
+});
+
+
+const botaoQuero = document.querySelector (".botao-quero");
+
+botaoQuero.addEventListener('click', function () {
+  console.log ('Cliquei em Quero');
+
+
+});
+
